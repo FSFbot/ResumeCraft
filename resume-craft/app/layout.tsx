e93d2b0d14a52f771b816d0756e74a847b3923ce
FSFbot <felipe.sfreitas14@gmail.com>
2025-05-01 19:08:38 -0300

    Estrutura do Projeto

---
+++ resume-craft/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import "../Styles/globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/shared/theme-provider";
+
+const fontSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" });
+const fontTitle = Nunito_Sans({ subsets: ["latin"], variable: "--font-title" });
+
+export const metadata: Metadata = {
+  title: "ResumeCraft",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontTitle.variable,
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ resume-craft/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="font-title">Welcome to Home Page</h1>
+    </main>
+  );
+}
