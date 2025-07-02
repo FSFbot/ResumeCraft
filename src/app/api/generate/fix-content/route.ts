a267c4f4003efa15bef326c1f08ab612661248af
FSFbot <felipe.sfreitas14@gmail.com>
2025-07-02 20:13:59 -0300

    versao de deploy agora funcionando

--- src/app/api/generate/fix-content/route.ts
+++ src/app/api/generate/fix-content/route.ts
@@ -21,7 +21,7 @@ export const POST = async (request: Request) => {
     const { content } = schema.parse(body);
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-4o",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
